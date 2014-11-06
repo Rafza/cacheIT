27381356a88b5d31b9a8e6cebce790c111d35b13
Alex Gian <AlexHGian@gmail.com>
2014-11-05 16:13:29 -0800

    Added Admin Dash Again

---
+++ client/app/admin_dashboard/dash.controller.js
@@ -0,0 +1,39 @@
+// dash.controller.js
+'use strict';
+angular.module('cacheItApp')
+  .controller('AdminDashCtrl', function ($scope, $location, Auth) {
+    $scope.items = ['teller'];
+    $scope.selection = $scope.items[0];
+
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.checkLogin = function() {
+      console.log("User is: " + $scope.isLoggedIn());
+      //console.log($scope.isActive('/logout'));
+      if($scope.isActive('/login') == false)
+      {
+          console.log( 'Logged Out' );
+          //$location.path('/');
+          return true;
+      }
+      return true;
+    };
+
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/');
+    };
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
---
+++ client/app/admin_dashboard/dash.js
@@ -0,0 +1,12 @@
+// dash.js
+'use strict';
+
+angular.module('cacheItApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin_dash', {
+        url: '/admin_dash',
+        templateUrl: 'app/admin_dashboard/dash.html',
+        controller: 'AdminDashCtrl'
+    });
+});
