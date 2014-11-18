22e57d9532eafd951be46e45e6bef428473b42d7
alex gian <alexhgian@gmail.com>
2014-11-18 14:01:26 -0800

    Teller Transfer Finished

--- client/app/admin_dashboard/dash_view/transfer.controller.js
+++ client/app/admin_dashboard/dash_view/transfer.controller.js
@@ -24,6 +24,7 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
   $scope.users = User.query();
   // begin same Account added
   $scope.accountType = ["Saving","Checking","Transfer"];
+  $scope.tranTypeOptions = ["Same Account","Different Account"];
   $scope.accOptionsFrom=['Checking', 'Saving'];
   $scope.accSelectedFrom = $scope.accOptionsFrom[0];
 
@@ -33,16 +34,17 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
   console.log("To saving-----t: "+$scope.accSelectedTo);
   $scope.errorAcc = false;
   $scope.errorAmt = false;
+  $scope.errorType = false;
   //finish old Account added
   $scope.errorFlag = false;
   $scope.errorFrom = false;
   $scope.errorTo = false;
   $scope.errorAmount = false;
   $scope.submitted = false;
-// 
+//
 // Begin Same Account
-// 
- $scope.confirmAcc = function() 
+//
+ $scope.confirmAcc = function()
   {
     $scope.submitted = true;
 
@@ -50,7 +52,7 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
   //  $scope.checkUsers($scope.user.emailFrom, $scope.user.emailTo, function(){});
   $q.all([
 
-    // begin new added 
+    // begin new added
     Auth.checkUser($scope.user.emailAcc, function(result) {
        console.log("checkUser() callback emailAcc: " + result);
        $scope.errorAcc = result;
@@ -59,42 +61,52 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
 
     if(!$scope.errorAcc)
     {
-      var a = "checking";
-      var b = "saving";
       var from = $scope.accSelectedFrom;
       var to = $scope.accSelectedTo;
       console.log("FROM ****"+from)
       console.log("TO ****"+to)
       var accID = data[0][0]._id;
-      if(a === from)
-      {
-      var fromOldAmt = data[0][0].checking;
-      var toOldAmt = data[0][0].Saving;
+
+      var toOldAmt = 0;
+      var fromOldAmt = 0;
+
+      if('Checking' == from){
+        fromOldAmt = data[0][0].checking;
+      } else {
+        fromOldAmt = data[0][0].saving;
       }
-      else 
-      {
-        console.log("didnt work man!!");
+      if('Checking' == to) {
+        toOldAmt = data[0][0].checking;
+      } else {
+        toOldAmt = data[0][0].saving;
       }
 
-      var amount = $scope.user.amountAcc;
+      var amount = $scope.user .amountAcc;
       console.log("from Amount"+fromOldAmt);
-      console.log("to Amount"+toOldAmt);
+      console.log("to Amount "+toOldAmt);
       // console.log("From same Account: "+from);
       // console.log("To same Account: "+to);
       // console.log("From same Account: "+amount);
     }
-      if(amount <= fromOldAmt) 
-      {
+      if(amount <= fromOldAmt && from != to) {
         console.log("data[0] :" + angular.toJson(accID) );
         var fromNewAmt = parseFloat(fromOldAmt)-parseFloat(amount);
 
         // BEGIN put function
-        $http.put('/api/users/' + accID + '/update',  {from : fromNewAmt } ).
+        var sendDataFrom;
+        if('Checking' == from) {
+          sendDataFrom = { checking : fromNewAmt };
+        } else{
+          sendDataFrom = { saving : fromNewAmt };
+        }
+        // console.log("New Amount to withdraw: " + sendDataFrom + " | " + fromNewAmt);
+        $http.put('/api/users/' + accID + '/update', sendDataFrom ).
         success(function(data, status, headers, config) {
           // this callback will be called asynchronously
           // when the response is available
           console.log("Success Withdraw! Returning new saving amount:");
-          console.log("NEW amount ON FROM"+data.from);
+          console.log("NEW amount ON FROM C" + data.checking);
+          console.log("NEW amount ON FROM S" + data.saving);
           //$scope.newChecking = data.checking;
           // usr.saving = data.saving;
           // TODO: return json to update only one row to reduce refreshing effect
@@ -107,8 +119,15 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
         console.log("data[1] :" + angular.toJson(accID) );
         var toNewAmt = parseFloat(toOldAmt)+parseFloat(amount);
 
+
+        var sendDataTo;
+        if('Checking' == from) {
+          sendDataTo = { checking : toNewAmt };
+        } else{
+          sendDataTo = { saving : toNewAmt };
+        }
         // BEGIN put function
-        $http.put('/api/users/' + accID + '/update',  { to : toNewAmt}).
+        $http.put('/api/users/' + accID + '/update', sendDataTo).
         success(function(data, status, headers, config) {
           // this callback will be called asynchronously
           // when the response is available
@@ -125,11 +144,13 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
           // or server returns response with an error status.
         });
         // END put function
-
+        $scope.errorType = false;
         $scope.errorAmt = false;
-      } 
-      else 
-      {
+      }
+      else if (from == to) {
+        console.log("Cannot transfer to the same account.");
+        $scope.errorType = true;
+      } else {
         console.log("Overdrawing fromUser");
         $scope.errorAmt = true;
       }
@@ -138,10 +159,10 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
 
 
   };
-// 
+//
 // End same Account
-// 
-  $scope.confirm = function() 
+//
+  $scope.confirm = function()
   {
     $scope.submitted = true;
 
@@ -159,7 +180,7 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
 
   ]).then(function(data) {
 
-    if(!$scope.errorFrom && !$scope.errorTo) 
+    if(!$scope.errorFrom && !$scope.errorTo)
     {
       var fromID = data[0][0]._id;
       var fromOldAmt = data[0][0].checking;
@@ -169,7 +190,7 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
       var amount = $scope.user.amount;
       // var overdraw = false;
 
-      if(amount <= fromOldAmt) 
+      if(amount <= fromOldAmt)
       {
         console.log("data[0] :" + angular.toJson(fromID) );
         var fromNewAmt = parseFloat(fromOldAmt)-parseFloat(amount);
@@ -213,8 +234,8 @@ myApp.controller('TranCtrl', function ($scope, $location, $http ,Auth, User, $q)
         // END put function
 
         $scope.errorAmount = false;
-      } 
-      else 
+      }
+      else
       {
         console.log("Overdrawing fromUser");
         $scope.errorAmount = true;
