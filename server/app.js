67d8c84ef2bb1b99e19545da925521931332b4ce
Azfar Alam <azfar500@gmail.com>
2014-12-08 12:11:33 -0800

    changed penalty time

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ var counter = 0;
 // Use Cron
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
-  cronTime: '*/1 * * * * *',
+  cronTime: '* */2 * * * *',
   onTick: function() {
     // console.log("Day " + (1+(++counter)%30) +" Updating Penalty/Interest...");
     controller.incrementDays();
